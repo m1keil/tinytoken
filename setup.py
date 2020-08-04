99b041f324ed3170bd18df29aadae91acc963f7a
Michael Sverdlik <m1keil@users.noreply.github.com>
2020-08-04 14:52:27 +1000

    fix setup.py

--- setup.py
+++ setup.py
@@ -5,4 +5,5 @@ setup(
     description="Command line tool to enable accessing AWS using federated GSuite single sign on",
     entry_points={"console_scripts": ["tinytoken=tinytoken.cli:main"]},
     install_requires=['requests'],
+    packages=find_packages(where='tinytoken')
 )
